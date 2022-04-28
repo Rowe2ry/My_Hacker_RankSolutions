a171e18b542efa5e209a7e2205a70443fda7b90c
Chris Rowe <rowe2ry@yahoo.com>
2022-04-28 16:11:24 -0400

    added contextual markdown file for Maximum Perimeterproblem.

---
+++ Maimum Perimeter/index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'maximumPerimeterTriangle' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY sticks as parameter.
+ */
+
+function maximumPerimeterTriangle(sticks) {
+    const sortedSticks = sticks.sort((a,b) => b - a);
+    let result = [];
+    for (let i = 0; i < sticks.length - 2; i++) {
+        if (sortedSticks[i] < (sortedSticks[i + 1] + sortedSticks[i + 2])) {
+            result.unshift(sortedSticks[i]);
+            result.unshift(sortedSticks[i + 1]);
+            result.unshift(sortedSticks[i + 2]);
+            return result;
+        };
+    };
+    return [-1];        
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const sticks = readLine().replace(/\s+$/g, '').split(' ').map(sticksTemp => parseInt(sticksTemp, 10));
+
+    const result = maximumPerimeterTriangle(sticks);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
