ee4b331e9b4a7e9885b2be3de9656626fd0ad04c
Chris Rowe <rowe2ry@yahoo.com>
2022-05-13 10:14:42 -0400

    Added a 2nd not working solution I came up with, and a working solution from the discussions of the site.

--- Gaming Array 1/index.js
+++ Gaming Array 1/index.js
@@ -29,18 +29,42 @@ function readLine() {
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
 
+// function gamingArray(arr) {
+//     let bob = true;
+//     while (arr.length > 0) {
+//         const arrCopy = arr.map((a) => a);
+//         const sort = arr.sort((a,b) => b-a);
+//         const greatest = sort[0];
+//         const position = arrCopy.indexOf(greatest);
+//         arr = arrCopy.slice(0,position);
+//         bob = !bob;
+//     };
+//     return bob ? 'ANDY' : 'BOB';
+// };
+
+// function gamingArray(arr) {
+//     let bob = true;
+//     while (arr.length > 0) {
+//         let position =0;
+//         const greatest = arr.reduce((a,b,index) => {
+//             let answer = a;
+//             if (a < b) {
+//                 answer = b;
+//                 position = index;
+//             };
+//             return answer;
+//         });
+//         arr = arr.slice(0,position);
+//         bob = !bob;
+//     };
+//     return bob ? 'ANDY' : 'BOB';
+// };
+
+// TODO: I didn't actually write this one. The 2 commented out ones above had good logic and worked, but weren't efficient enough.
 function gamingArray(arr) {
-    let bob = true;
-    while (arr.length > 0) {
-        const arrCopy = arr.map((a) => a);
-        const sort = arr.sort((a,b) => b-a);
-        const greatest = sort[0];
-        const position = arrCopy.indexOf(greatest);
-        arr = arrCopy.slice(0,position);
-        bob = !bob;
-    };
-    return bob ? 'ANDY' : 'BOB';
-};
+    let m = 0
+    return (arr.reduce((c,v) => v>m ? (m=v, c+1) : c, 0)) % 2 ? 'BOB' : 'ANDY'
+}
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
