82191002b13c4047eac3c5f65f747ba28a784ba1
Chris Rowe <rowe2ry@yahoo.com>
2022-09-01 15:46:01 -0400

    added palindrome number solution

--- LeetCodeProblems/TwoSum/solution.js
+++ LeetCodeProblems/TwoSum/solution.js
@@ -5,7 +5,7 @@
  */
 
 /* ========================================================
- * My original soution (brute force)
+ * My original soution (brute force | 169ms | faster than 33.20% of LC submissions) 
  * ======================================================== */
 
  var twoSum = function(nums, target) {
@@ -23,6 +23,34 @@
 };
 
 /* ========================================================
- * More optimized from discussions
+ * More optimized from discussions (hashmap | 132ms | faster than 47.62% of solutions)
  * ======================================================== */
 
+var twoSum = function(nums, target) {
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            return [map.get(nums[i]), i];
+        }
+
+        map.set(target - nums[i], i);
+    };
+};
+
+/* ========================================================
+ * More optimized from discussions (hashmap #2 | 115ms | faster than 58.79% of solutions)
+ * ======================================================== */
+
+var twoSum = function(nums, target) {
+    
+    const hash = new Map();
+    
+    for(let i = 0; i < nums.length; i++){
+        
+        if(hash.has(nums[i])) return [i, hash.get(nums[i])];
+        
+        else hash.set(target - nums[i], i);
+        
+    };    
+};
