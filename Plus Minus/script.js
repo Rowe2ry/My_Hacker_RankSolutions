4594b2c2dc5760fe4babbc7ac0f63fa7ca1a3df3
Chris Rowe <rowe2ry@yahoo.com>
2022-09-21 10:12:49 -0400

    refactored Plus Minus Solution

--- Plus Minus/script.js
+++ Plus Minus/script.js
@@ -28,11 +28,11 @@ function readLine() {
 
 function plusMinus(arr) {
     // Write your code here
-    const intNum = arr.length;
+    const len = arr.length; // referenced several times, shorter
     let positives = 0;
     let negatives = 0;
     let zeros = 0;
-    for (let i = 0; i < intNum; i++) {
+    for (let i = 0; i < len; i++) {
         if(arr[i] > 0){
             positives++;
         } else if (arr[i] < 0) {
@@ -42,40 +42,35 @@ function plusMinus(arr) {
         };
     };
     let preciseArray = [];
-    let posFract = positives / intNum;
-    let negFract = negatives / intNum;
-    let zerFract = zeros / intNum;
+    let posFract = positives / len;
+    let negFract = negatives / len;
+    let zerFract = zeros / len;
     let deciArray = [posFract, negFract, zerFract];
-    for (let i = 0; i < deciArray.length ; i++) {
-        let thisNum = deciArray[i];
-        thisNum = thisNum.toString();
-        if (thisNum.length == 1) {
-            let modifierString = '.000000';
-            let stringValue = thisNum.toString();
-            stringValue = stringValue.concat(modifierString);
+    for (let num of deciArray) {
+        num = num.toString(); // numbers will drop trailing zeros in JS
+        if (num.length == 1) { //0
+            let modifierString = '.000000'; // a decimal and 6 zeros
+            const stringValue = num.concat(modifierString); // 0.000000
             preciseArray.push(stringValue);
-        } else if (thisNum.length < 9) {
-            let difference = 8 - thisNum.length;
+        } else if (num.length < 9) { // too short; 8 or less (8 is fine actually)
+            let difference = 8 - num.length; // how much too short?
             let modifierString = '';
-            for (let j = 0; j < difference; j++) {
+            for (let j = 0; j < difference; j++) { // add that many zeros
                 modifierString = modifierString.concat('0');
             };
-            let stringValue = thisNum.toString();
-            stringValue = stringValue.concat(modifierString);
+            const stringValue = num.concat(modifierString);
             preciseArray.push(stringValue);
-        } else if (thisNum.length > 8) {
-            while (thisNum.length > 8) {
-                thisNum = thisNum.slice(0,-1);
+        } else if (num.length > 8) { // too big, 9 or more
+            while (num.length > 8) { // take one down, pass it around...
+                num = num.slice(0,-1); // start at index and end at length - 1, ie. removes the last char
             };
-            let stringValue = thisNum.toString();
-            preciseArray.push(stringValue);
+            preciseArray.push(num);
         } else {
-        let stringValue = thisNum.toString();
-        preciseArray.push(stringValue);
+        preciseArray.push(num);
         };
     };
-    for (let i = 0; i < preciseArray.length; i++) {
-        console.log(preciseArray[i]);
+    for (const answer of preciseArray) { // loop through answers and print them
+        console.log(answer);
     };
 };
 
